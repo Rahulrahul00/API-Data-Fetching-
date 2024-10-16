4a14ecef165c56dcabe0af0b6309aba5eb2806d5
Rahul c shaji <rahulcshaji007@gmail.com>
2024-10-16 22:48:25 +0530

    Model Popup Box completed.Clic when you click the eyeicon ,the background will blur

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function getData() {
                    <td>${item.age}</td>
                    <td>${item.gender}</td>
                    <td class="email-column">${item.email}</td>
-                  <td onclick='eyeView(${JSON.stringify(item)})'><i class="fa-solid fa-eye"></i></td>
+                  <td class="eye" onclick='eyeView(${JSON.stringify(item)})'><i class="fa-solid fa-eye fa-beat fa-lg"></i></td>
                 </tr>`
     });
 
@@ -28,7 +28,62 @@ async function getData() {
 
 
     function eyeView(item){
-        alert(`User:${item.firstName} ${item.lastName} profile : ${item.image} Age : ${item.age}`)
+
+      let displayDiv = document.getElementById("displayDiv");
+      let mainContent = document.getElementById("mainContent"); // Select the content to blur
+
+
+      displayDiv.innerHTML = '';
+
+      let employeDiv = document.createElement("div");
+          employeDiv.classList.add('popup');
+
+         // Adding the 'open-popup' class to the created popup element
+          employeDiv.classList.add('open-popup');
+
+          
+
+          employeDiv.innerHTML = `
+                        <h5 class="profile"><img src="${item.image}" ></h5>
+                        <h4>${item.firstName} ${item.lastName}</h4>
+                        <h5 class="age">Age : ${item.age} </h5>
+                        <h5 class="gender">Gender : ${item.gender} </h5>
+                        <h5 class="phone">Phone : ${item.phone} </h5>
+                        <h5 class="mail">Email : ${item.email}</h5>
+                        <h5 class="bloodGroup">bloodGroup : ${item.bloodGroup}</h5>
+                        <button class="closeBtn" onclick="closePopup()">Close</button>
+
+                        
+                        
+
+          `;
+             
+          
+            displayDiv.appendChild(employeDiv);    
+            
+            
+            setTimeout(() => {
+              employeDiv.classList.add('open-popup'); // Add 'open-popup' class after a tiny delay
+              mainContent.classList.add('blurred'); // Add the blur effect to the main content
+            }, 10);
+          
+            
+
+        // alert(`User:${item.firstName} ${item.lastName} profile : ${item.image} Age : ${item.age}`)
     }
 
 
+    function closePopup() {
+      // Find the active popup and remove the 'open-popup' class
+      let activePopup = document.querySelector('.popup.open-popup');
+      let mainContent = document.getElementById("mainContent");
+      if (activePopup) {
+        activePopup.classList.remove('open-popup');
+
+        mainContent.classList.remove('blurred');
+
+        setTimeout(() => {
+          activePopup.remove(); // Remove element after transition completes
+        }, 400);
+      }
+    }
