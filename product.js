ad9755a360ad5a048364cc6bc992f3731600cd96
Rahul c shaji <rahulcshaji007@gmail.com>
2024-10-18 18:15:28 +0530

    Completed the UI alginment for product images,title,price,category and decscription, and made it responsive for different device sizes

--- product.js
+++ product.js
@@ -20,18 +20,26 @@ async function productData() {
               products.innerHTML += `
     <div class="product">
              <img src="${items.images[0]}" class="product-img">
+
+        <div class="product-content">
             <h2 class="product-title">${title.length >10 ? title.substring(0, 10).concat('...') : title}</h2>
             <h4 class="product-category">${items.category}</h4>
-            <p class="product-description">${description.length > 80 ? description.substring(0, 80).concat('...more') : description }</p>
+            <p class="product-description">${description.length > 40 ? description.substring(0, 40).concat('...more') : description }</p>
             <div class="product-price-container">
-                <h3 class="product-price">${items.price}</h3>
-                <a href="#" data-productId="${items.id}" class="add-to-cart"></a>
+                <h3 class="product-price">Rs ${items.price}</h3>
+                <a href="#" data-productId="${items.id}" class="add-to-cart">Add To Cart</a>
             </div>
-        </div>
+                
+            </div>
+          
+        </div>  
 `
 
-        console.log(store);
+        // console.log(store);
     
     });
+
+    
     
 }
+
