d812a4873020d72ccb69d11ef76821eba2b81368
Rahul c shaji <rahulcshaji007@gmail.com>
2024-10-19 23:27:47 +0530

    Home page created for nav bar and banner section

--- product.js
+++ product.js
@@ -9,70 +9,89 @@ async function productData() {
 
     const store = await data.json();
 
+    searchItems(store.products);  // Render products to display all products
+
+}
+
     // console.log(store);
 
-   store.products.forEach(items =>{
+    let searchItems = (items) =>{
+        products.innerHTML = ''; //clear the previous products
 
 
-   let description = items.description;
-   let title = items.title;
-   let price = items.price;
-      
-  
-              products.innerHTML += `
-    <div class="product">
-             <img src="${items.images[0]}" class="product-img">
+        items.forEach(item =>{
 
-        <div class="product-content">
-            <h2 class="product-title">${title.length >10 ? title.substring(0, 10).concat('...') : title}</h2>
-            <h4 class="product-category">${items.category}</h4>
-            <p class="product-description">${description.length > 40 ? description.substring(0, 40).concat('...more') : description }</p>
-            <div class="product-price-container">
-                <h3 class="product-price">Rs ${price}</h3>
-                <a href="#!" data-productId="${items.id}" class="add-to-cart">Add To Cart</a>
-            </div>
-                
-            </div>
-          
-        </div>  
-      `
-});
+            let description = item.description;
+            let title = item.title;
+            let price = item.price;
+               
+           
+                       products.innerHTML += `
+             <div class="product">
+                      <img src="${item.images[0]}" class="product-img">
+         
+                 <div class="product-content">
+                     <h2 class="product-title">${title.length >10 ? title.substring(0, 10).concat('...') : title}</h2>
+                     <h4 class="product-category">${item.category}</h4>
+                     <p class="product-description">${description.length > 40 ? description.substring(0, 40).concat('...more') : description }</p>
+                     <div class="product-price-container">
+                         <h3 class="product-price">Rs ${price}</h3>
+                         <a href="#!" data-productId="${item.id}" class="add-to-cart">Add To Cart</a>
+                     </div>
+                         
+                     </div>
+                   
+                 </div>  
+               `;
+         });
 
-// search products
 
+    }
 
+   
 
-document.getElementById("search").addEventListener('click', async ()=>{
+// search products when user click the search button
+
+document.getElementById('searchBtn').addEventListener('click', async ()=>{
 
     let searchInput = document.getElementById('search-input').value;
 
     if(searchInput.trim() !== ''){
 
-     
-        
         const searchResponse = await fetch(`https://dummyjson.com/products/search?q=${searchInput}`);
-
-
         const searchData = await searchResponse.json();
 
         if(searchData.products.length > 0){
-            renderProducts(searchData.products);
-        }else{
-            products.innerHTML = '<p> No products found.</p>';
-        }
+            searchItems(searchData.products); //display the seatch result
 
+
+        }else{
+            products.innerHTML ='<p>No product found.</P>';
+        }
     }else{
-        productData();
+        productData(); // reload all products if search input is empty
     }
 
-
+    // console.log(searchInput);
 
 });
 
+productData();
+
+
+
+
+
+     
+        
+        // const searchResponse = await fetch(`https://dummyjson.com/products/search?q=${searchInput}`);
+
+
+     
     
 
     
     
-}
+
 
 
