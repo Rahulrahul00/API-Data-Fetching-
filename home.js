35f3ce1a304b55ad6429839dae8b68aeceadd7be
Rahul c shaji <rahulcshaji007@gmail.com>
2024-10-20 18:20:54 +0530

    navbar fixed aanimation set

--- home.js
+++ home.js
@@ -1,2 +1,11 @@
 // fix Headers
 
+window.addEventListener('scroll', function(){
+    var header = document.getElementById('header');
+    if(window.scrollY > 0){
+        header.classList.add('header-fix');
+    }else{
+        header.classList.remove('header-fix');
+    }
+
+});
